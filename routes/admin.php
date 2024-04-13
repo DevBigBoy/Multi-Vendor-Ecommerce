d99fc74512746f85686fd3f86c8434adffcfd9a5
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-04-13 05:45:25 +0200

    Create separate route files for admin and vendor

---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AdminController;
+
+/**
+ * Admin Routes
+ */
+Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
---
+++ routes/vendor.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\VendorController;
+
+/**
+ * Vendor Routes
+ */
+Route::get('dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
