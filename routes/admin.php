31a784a69046955c8cb362f4bda121a72a5eb287
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-04-23 05:45:25 +0200

    Add Slider Page Design

--- routes/admin.php
+++ routes/admin.php
@@ -3,13 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 
-/**
- * Admin Routes
- */
+/** Admin Routes */
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
 
 /** Profile Routes */
 Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 Route::put('profile/update/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+
+/** Slider Routes */
+Route::resource('slider', SliderController::class);
