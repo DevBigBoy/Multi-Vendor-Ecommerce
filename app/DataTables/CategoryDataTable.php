aa6b65971860f61c30dd9cdf75be766976eb1024
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-04-29 12:45:25 +0300

    Category - show data in index page

--- app/DataTables/CategoryDataTable.php
+++ app/DataTables/CategoryDataTable.php
@@ -14,71 +14,87 @@ use Yajra\DataTables\Services\DataTable;
 
 class CategoryDataTable extends DataTable
 {
-    /**
-     * Build the DataTable class.
-     *
-     * @param QueryBuilder $query Results from query() method.
-     */
-    public function dataTable(QueryBuilder $query): EloquentDataTable
-    {
-        return (new EloquentDataTable($query))
-            ->addColumn('action', 'category.action')
-            ->setRowId('id');
-    }
+  /**
+   * Build the DataTable class.
+   *
+   * @param QueryBuilder $query Results from query() method.
+   */
+  public function dataTable(QueryBuilder $query): EloquentDataTable
+  {
+    return (new EloquentDataTable($query))
+      ->addColumn('action', function ($query) {
+        $editBtn   = "<a href='" .  route('admin.category.edit',  $query->id) . "' class='btn btn-primary'> <i class='far fa-edit'></i> Edit</a>";
+        $deleteBtn = "<a href='" .  route('admin.category.destroy',  $query->id) . "' class='btn btn-danger ml-2 delete-item'> <i class='fas fa-trash'></i> Delete</a>";
+        return $editBtn . $deleteBtn;
+      })
+      ->addColumn('icon', function ($query) {
+        $icon = "<i style='font-size:40px;' class='" . $query->icon . "'></i>";
+        return $icon;
+      })
+      ->addColumn('status', function ($query) {
+        if ($query->status == 1) {
+          return "<div class='badge badge-success'>Active</div>";
+        } elseif ($query->status == 0) {
+          return "<div class='badge badge-danger'>Not Active</div>";
+        }
+      })
+      ->rawColumns(['action', 'icon', 'status'])
+      ->setRowId('id');
+  }
 
-    /**
-     * Get the query source of dataTable.
-     */
-    public function query(Category $model): QueryBuilder
-    {
-        return $model->newQuery();
-    }
+  /**
+   * Get the query source of dataTable.
+   */
+  public function query(Category $model): QueryBuilder
+  {
+    return $model->newQuery();
+  }
 
-    /**
-     * Optional method if you want to use the html builder.
-     */
-    public function html(): HtmlBuilder
-    {
-        return $this->builder()
-                    ->setTableId('category-table')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    //->dom('Bfrtip')
-                    ->orderBy(1)
-                    ->selectStyleSingle()
-                    ->buttons([
-                        Button::make('excel'),
-                        Button::make('csv'),
-                        Button::make('pdf'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload')
-                    ]);
-    }
+  /**
+   * Optional method if you want to use the html builder.
+   */
+  public function html(): HtmlBuilder
+  {
+    return $this->builder()
+      ->setTableId('category-table')
+      ->columns($this->getColumns())
+      ->minifiedAjax()
+      //->dom('Bfrtip')
+      ->orderBy(0)
+      ->selectStyleSingle()
+      ->buttons([
+        Button::make('excel'),
+        Button::make('csv'),
+        Button::make('pdf'),
+        Button::make('print'),
+        Button::make('reset'),
+        Button::make('reload')
+      ]);
+  }
 
-    /**
-     * Get the dataTable columns definition.
-     */
-    public function getColumns(): array
-    {
-        return [
-            Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
-        ];
-    }
+  /**
+   * Get the dataTable columns definition.
+   */
+  public function getColumns(): array
+  {
+    return [
+      Column::make('id'),
+      Column::make('icon'),
+      Column::make('name'),
+      Column::make('slug'),
+      Column::make('status'),
+      Column::computed('action')
+        ->exportable(false)
+        ->printable(false)
+        ->addClass('text-center'),
+    ];
+  }
 
-    /**
-     * Get the filename for export.
-     */
-    protected function filename(): string
-    {
-        return 'Category_' . date('YmdHis');
-    }
+  /**
+   * Get the filename for export.
+   */
+  protected function filename(): string
+  {
+    return 'Category_' . date('YmdHis');
+  }
 }
